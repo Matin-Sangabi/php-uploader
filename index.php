34f8c2c24c0ff7f34e48416337639615d4617fcb
Matin-sangabi <sangabiwork@gmail.com>
2022-12-04 13:14:00 -0800

    hack fixed

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
     <form action="process.php" method ="post" enctype="multipart/form-data">
         <input type="text" name="name" /><br/>
         upload file : 
         <input type ="file" name="pic" /><br/>
         <button type="submit">Login</button>
     </form> 
-</body>
-</html>
