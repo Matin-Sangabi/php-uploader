6cdef78d45d5ada4f40b56b948f6741c384c8b3d
Matin-sangabi <sangabiwork@gmail.com>
2022-12-04 13:11:41 -0800

    gitignore added

--- process.php
+++ process.php
@@ -1,11 +1,11 @@
 <?php 
     $pic_name = $_FILES['pic']['name'];
     $name = $_POST['name'];
-    mkdir("upload/".$name);
+    mkdir("uploader/".$name);
     $array = explode("." , $pic_name); // like split in js => 1.png =>out => [1][png]
     $ext = end($array); // png
     $new_name = rand().".".$ext;
     $from = $_FILES['pic']['tmp_name'];
-    $to = "upload/".$name."/".$new_name;
+    $to = "uploader/".$name."/".$new_name;
     move_uploaded_file($from , $to);
     
